42496c2654b9e5f00c4b4d3120e4aa59e825d5c8
Florin Papa <florin.papa192@gmail.com>
2015-04-27 11:39:42 +0300

    Solve lab tasks

--- labtasks/01-calculatorwebservice/src/ro/pub/cs/systems/pdsd/lab07/calculatorwebservice/graphicaluserinterface/CalculatorWebServiceActivity.java
+++ labtasks/01-calculatorwebservice/src/ro/pub/cs/systems/pdsd/lab07/calculatorwebservice/graphicaluserinterface/CalculatorWebServiceActivity.java
@@ -1,8 +1,29 @@
 package ro.pub.cs.systems.pdsd.lab07.calculatorwebservice.graphicaluserinterface;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
 import ro.pub.cs.systems.pdsd.lab07.calculatorwebservice.R;
+import ro.pub.cs.systems.pdsd.lab07.calculatorwebservice.general.Constants;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,13 +45,128 @@ public class CalculatorWebServiceActivity extends Activity {
 			
 			// TODO: exercise 4
 			// get operators 1 & 2 from corresponding edit texts (operator1EditText, operator2EditText)
+			EditText op1_edit = (EditText)findViewById(R.id.operator1_edit_text);
+			String op1 = op1_edit.getText().toString();
+			
+			EditText op2_edit = (EditText)findViewById(R.id.operator2_edit_text);
+			String op2 = op2_edit.getText().toString();
+			
 			// signal missing values through error messages
+			if (op1 == null || op2 == null) {
+				Log.e("ERR", "Null ooperators");
+				return;
+			}
+			
 			// get operation from operationsSpinner
+			Spinner op_spinner = (Spinner) findViewById(R.id.operations_spinner);
+			String operation = op_spinner.getSelectedItem().toString();
 			
 			// create an instance of a HttpClient object
+			HttpClient httpClient = new DefaultHttpClient();
 			
 			// get method used for sending request from methodsSpinner
+			Spinner meth_sp = (Spinner) findViewById(R.id.methods_spinner);
+			String method = meth_sp.getSelectedItem().toString();
 			
+			HttpResponse response = null;
+			if (method.equals("GET")) {
+				Log.e("Err", "Sending GET request");
+				HttpGet httpGet = new HttpGet(Constants.GET_WEB_SERVICE_ADDRESS
+                        + "?" + Constants.OPERATION_ATTRIBUTE + "=" + operation
+                        + "&" + Constants.OPERATOR1_ATTRIBUTE + "=" + op1
+                        + "&" + Constants.OPERATOR2_ATTRIBUTE + "=" + op2);
+				try {
+					response = httpClient.execute(httpGet);
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				final HttpEntity httpGetEntity = response.getEntity();
+				if (httpGetEntity != null) {  
+				    // do something with the response
+				    final TextView tv = (TextView) findViewById(R.id.result_text_view);
+				    try {
+						tv.post(new Runnable() {
+							
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								try {
+									String s = EntityUtils.toString(httpGetEntity);
+									Log.e("Err", "Got response " + s);
+									tv.setText(s);
+								} catch (ParseException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							}
+						});
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			} else {
+				HttpPost httpPost = new HttpPost(Constants.POST_WEB_SERVICE_ADDRESS);
+				List<NameValuePair> params = new ArrayList<NameValuePair>();        
+				params.add(new BasicNameValuePair(Constants.OPERATION_ATTRIBUTE, operation));
+				params.add(new BasicNameValuePair(Constants.OPERATOR1_ATTRIBUTE, op1));
+				params.add(new BasicNameValuePair(Constants.OPERATOR2_ATTRIBUTE, op2));
+				try {
+				  UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+				  httpPost.setEntity(urlEncodedFormEntity);
+				} catch (UnsupportedEncodingException unsupportedEncodingException) {
+				  Log.e(Constants.TAG, unsupportedEncodingException.getMessage());
+				  if (Constants.DEBUG) {
+				    unsupportedEncodingException.printStackTrace();
+				  }						
+				}
+				try {
+					response = httpClient.execute(httpPost);
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				final HttpEntity httpGetEntity = response.getEntity();
+				if (httpGetEntity != null) {  
+				    // do something with the response
+				    final TextView tv = (TextView) findViewById(R.id.result_text_view);
+				    try {
+						tv.post(new Runnable() {
+							
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								try {
+									String s = EntityUtils.toString(httpGetEntity);
+									Log.e("Err", "Got response " + s);
+									tv.setText(s);
+								} catch (ParseException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							}
+						});
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
 			// 1. GET
 			// a) build the URL into a HttpGet object (append the operators / operations to the Internet address)
 			// b) create an instance of a ResultHandler object
@@ -42,9 +178,10 @@ public class CalculatorWebServiceActivity extends Activity {
 			// c) create an instance of a UrlEncodedFormEntity object using the list and UTF-8 encoding and attach it to the post request
 			// d) create an instance of a ResultHandler object
 			// e) execute the request, thus generating the result
+			Log.e("Err", "here here");
 			
 			// display the result in resultTextView
-			
+			            
 		}
 	}
 	
--- labtasks/02-xkcdcartoondisplayer/src/ro/pub/cs/systems/pdsd/lab07/xkcdcartoondisplayer/graphicuserinterface/XkcdCartoonDisplayerActivity.java
+++ labtasks/02-xkcdcartoondisplayer/src/ro/pub/cs/systems/pdsd/lab07/xkcdcartoondisplayer/graphicuserinterface/XkcdCartoonDisplayerActivity.java
@@ -1,11 +1,27 @@
 package ro.pub.cs.systems.pdsd.lab07.xkcdcartoondisplayer.graphicuserinterface;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import ro.pub.cs.systems.pdsd.lab07.xkcdcartoondisplayer.R;
 import ro.pub.cs.systems.pdsd.lab07.xkcdcartoondisplayer.entities.XkcdCartoonInfo;
 import ro.pub.cs.systems.pdsd.xkcdcartoondisplayer.general.Constants;
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -39,7 +55,7 @@ public class XkcdCartoonDisplayerActivity extends Activity {
 		@Override
 		protected XkcdCartoonInfo doInBackground(String... urls) {
 			
-			XkcdCartoonInfo xkcdCartoonInfo = new XkcdCartoonInfo();
+			final XkcdCartoonInfo xkcdCartoonInfo = new XkcdCartoonInfo();
 			
 			// TODO: exercise 5a)
 			// 1. obtain the content of the web page (whose Internet address is stored in urls[0])
@@ -47,14 +63,52 @@ public class XkcdCartoonDisplayerActivity extends Activity {
 			// - create an instance of a HttpGet object
 			// - create an instance of a ResponseHandler object
 			// - execute the request, thus obtaining the web page source code
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpGet httpGet = new HttpGet(urls[0]);
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			String content = "";
+			try {
+				content = httpClient.execute(httpGet, responseHandler);
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 			// 2. parse the web page source code
 			// - cartoon title: get the tag whose id equals "ctitle"
+			Document document = Jsoup.parse(content);
+			Element htmlTag = document.child(0);
+			Element divTagIdCtitle = htmlTag.getElementsByAttributeValue(Constants.ID_ATTRIBUTE, Constants.CTITLE_VALUE).first();
+			xkcdCartoonInfo.setCartoonTitle(divTagIdCtitle.ownText());
+			
 			// - cartoon url
 			//   * get the first tag whose id equals "comic"
 			//   * get the embedded <img> tag
 			//   * get the value of the attribute "src"
 			//   * prepend the protocol: "http:"
+			Element divTagIdComic = htmlTag.getElementsByAttributeValue(Constants.ID_ATTRIBUTE, Constants.COMIC_VALUE).first();
+			String cartoonInternetAddress = divTagIdComic.getElementsByTag(Constants.IMG_TAG).attr(Constants.SRC_ATTRIBUTE);
+			cartoonInternetAddress = Constants.HTTP_PROTOCOL + cartoonInternetAddress;
+			xkcdCartoonInfo.setCartoonUrl(cartoonInternetAddress);
+			
+			Log.e("Err", cartoonInternetAddress);
+			try {
+				HttpGet httpGet2 = new HttpGet(cartoonInternetAddress);
+				HttpResponse response = httpClient.execute(httpGet2);
+				HttpEntity httpGetEntity = response.getEntity();
+				if (httpGetEntity != null) {
+					xkcdCartoonInfo.setCartoonContent(BitmapFactory.decodeStream(httpGetEntity.getContent()));
+				}
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			// - cartoon content: get the input stream attached to the url and decode it into a Bitmap
 			// - previous cartoon address
 			//   * get the first tag whole rel attribute equals "prev"
@@ -66,6 +120,15 @@ public class XkcdCartoonDisplayerActivity extends Activity {
 			//   * get the href attribute of the tag
 			//   * prepend the value with the base url: http://www.xkcd.com
 			//   * attach the next button a click listener with the address attached
+			Element aTagRelPrev = htmlTag.getElementsByAttributeValue(Constants.REL_ATTRIBUTE, Constants.PREVIOUS_VALUE).first();
+			String previousCartoonInternetAddress = Constants.XKCD_INTERNET_ADDRESS + aTagRelPrev.attr(Constants.HREF_ATTRIBUTE);
+//			previousCartoonInternetAddress = previousCartoonInternetAddress;
+			xkcdCartoonInfo.setPreviousCartoonUrl(previousCartoonInternetAddress);
+			
+			Element aTagRelNext = htmlTag.getElementsByAttributeValue(Constants.REL_ATTRIBUTE, Constants.NEXT_VALUE).first();
+			String nextCartoonInternetAddress = Constants.XKCD_INTERNET_ADDRESS + aTagRelNext.attr(Constants.HREF_ATTRIBUTE);
+//			nextCartoonInternetAddress = Constants.HTTP_PROTOCOL + nextCartoonInternetAddress;
+			xkcdCartoonInfo.setNextCartoonUrl(nextCartoonInternetAddress);
 			
 			return xkcdCartoonInfo;
 
@@ -79,7 +142,12 @@ public class XkcdCartoonDisplayerActivity extends Activity {
 			// cartoonTitle -> xkcdCartoonTitleTextView
 			// cartoonContent -> xkcdCartoonImageView
 			// cartoonUrl -> xkcdCartoonUrlTextView
-
+			xkcdCartoonImageView.setImageBitmap(xkcdCartoonInfo.getCartoonContent());
+			xkcdCartoonTitleTextView.setText(xkcdCartoonInfo.getCartoonTitle());
+			xkcdCartoonUrlTextView.setText(xkcdCartoonInfo.getCartoonUrl());
+			
+			previousButton.setOnClickListener(new XkcdCartoonUrlButtonClickListener(xkcdCartoonInfo.getPreviousCartoonUrl()));
+			nextButton.setOnClickListener(new XkcdCartoonUrlButtonClickListener(xkcdCartoonInfo.getNextCartoonUrl()));
 		}
 		
 	}
